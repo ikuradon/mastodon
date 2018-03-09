084dd8ae00511bd2b4dde8eff456fdce31180599
ikuradon <hawaiianphoto@geekhost.net>
2018-03-09 21:42:31 +0900

    Fix something.

--- update.sh
+++ update.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 set -eu
-
 source /etc/profile.d/nvm.sh
+export RAILS_ENV=production
+
 nvm use
 for cmds in bundle yarn;do if ! type ${cmds} 2>/dev/null 1>/dev/null;then echo "${cmds}: Not found";exit 1;fi;done
 
 cd `dirname $0`
 
-export RAILS_ENV=production
+for pidfile in `ls tmp/pids/sidekiq-*`;do bundle exec sidekiqctl quiet $pidfile;done
 
 git fetch --all
-git merge --no-edit --progress upstream/master
+git merge --no-commit --progress upstream/master
 
 ret=$?
 if [ $ret -ne 0 ];then
 echo "Merge error"
+git merge --abort
+for pidfile in `ls tmp/pids/sidekiq-*`;do bundle exec sidekiqctl stop $pidfile;done
 exit 1
 fi
 
+git commit -c "Merge remote-tracking branch 'upstream/master' into comm.cx"
 git push -u origin comm.cx
 
 bundle install --path=vendor/bundle --without development test --retry=3 --jobs=5
 yarn --pure-lockfile && yarn cache clean
 
-for pidfile in `ls tmp/pids/sidekiq-*`;do bundle exec sidekiqctl quiet $pidfile;done
-
 bin/rails db:migrate
-bin/rails assets:precompile
+
+for pidfile in `ls tmp/pids/sidekiq-*`;do bundle exec sidekiqctl stop $pidfile;done
+pkill -u `id -u` node
+
+time bin/rails assets:precompile
 bin/rails comm:revwrite
 
 passenger-config restart-app --rolling-restart .
-for pidfile in `ls tmp/pids/sidekiq-*`;do bundle exec sidekiqctl stop $pidfile;done
-pkill -u `id -u` node
