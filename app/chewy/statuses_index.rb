bb41f830078832dfa77fc3e61b0a8bce0c2d14c0
ikuradon <hawaiianphoto@geekhost.net>
2018-04-12 23:45:21 +0900

    Modify chewy logic.

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -16,8 +16,39 @@ class StatusesIndex < Chewy::Index
         language: 'possessive_english',
       },
     },
+    tokenizer: {
+      ja_ma_tokenizer: {
+        type: 'sudachi_tokenizer',
+        mode: 'search',
+        discard_punctuation: 'true',
+        resources_path: '/etc/elasticsearch/sudachi',
+        settings_path: '/etc/elasticsearch/sudachi/sudachi.json'
+      },
+      ja_ngram_tokenizer: {
+        type: 'nGram',
+        min_gram: '2',
+        max_gram: '3',
+      },
+    },
     analyzer: {
       content: {
+        type: 'custom',
+        tokenizer: 'ja_ma_tokenizer',
+        filter: %w(
+          sudachi_baseform
+          cjk_width
+          lowercase
+        ),
+      },
+      ngram: {
+        type: 'custom',
+        tokenizer: 'ja_ngram_tokenizer',
+        filter: %w(
+          cjk_width
+          lowercase
+        ),
+      },
+      uue: {
         tokenizer: 'uax_url_email',
         filter: %w(
           english_possessive_stemmer
@@ -52,6 +83,8 @@ class StatusesIndex < Chewy::Index
 
       field :text, type: 'text', value: ->(status) { [status.spoiler_text, Formatter.instance.plaintext(status)].join("\n\n") } do
         field :stemmed, type: 'text', analyzer: 'content'
+        field :ngram, type: 'text', analyzer: 'ngram'
+        field :uue, type: 'text', analyzer: 'uue'
       end
 
       field :searchable_by, type: 'long', value: ->(status, crutches) { status.searchable_by(crutches) }
