3a65ddaa06792a3a5768ba9940522dadbbc8d8d1
ikuradon <hawaiianphoto@geekhost.net>
2019-05-06 00:12:25 +0900

    Revert "[New] Implement a feature of quote"
    This reverts commit 9691485427e9dfa3284c76ded1cdb1c7c1cdd9c5.

--- app/javascript/mastodon/actions/compose.js
+++ app/javascript/mastodon/actions/compose.js
@@ -149,7 +149,6 @@ export function submitCompose(routerHistory) {
   return function (dispatch, getState) {
     let status = getState().getIn(['compose', 'text'], '');
     const media  = getState().getIn(['compose', 'media_attachments']);
-    const quoteId = getState().getIn(['compose', 'quote_from'], null);
 
     if ((!status || !status.length) && media.size === 0) {
       return;
@@ -162,13 +161,6 @@ export function submitCompose(routerHistory) {
         status = [status, ` #${tag.get('text')}`].join('')
       }
     });
-    if (quoteId) {
-      status = [
-        status,
-        "~~~~~~~~~~",
-        `[${quoteId}][${getState().getIn(['compose', 'quote_from_uri'], null)}]`
-      ].join("\n");
-    }
 
     dispatch(submitComposeRequest());
 
