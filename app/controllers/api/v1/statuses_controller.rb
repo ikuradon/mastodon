005f1aaeca10612bed9c622b8fd14d8a39dc8f90
ikuradon <hawaiianphoto@geekhost.net>
2020-03-16 23:49:09 +0900

    Fix typo

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -45,7 +45,7 @@ class Api::V1::StatusesController < Api::BaseController
                                          application: doorkeeper_token.application,
                                          poll: status_params[:poll],
                                          idempotency: request.headers['Idempotency-Key'],
-                                         with_rate_limit: true),
+                                         with_rate_limit: true,
                                          quote_id: status_params[:quote_id].blank? ? nil : status_params[:quote_id])
 
     render json: @status, serializer: @status.is_a?(ScheduledStatus) ? REST::ScheduledStatusSerializer : REST::StatusSerializer
